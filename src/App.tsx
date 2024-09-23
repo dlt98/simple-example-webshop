5222ae30388a10eebb255f22791ef0622bf1057e
David L. Tomasovic <davidleo.toma@gmail.com>
2024-09-23 21:54:51 +0200

    Add grid layout to product card

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   if (isFetching) return <div>THIS IS FETCHING</div>;
 
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {data?.products.map(({ description, title, price, thumbnail }) => {
         return (
           <ProductCard
