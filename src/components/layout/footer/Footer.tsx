d07e4a740bcfb63360465103443587b13ef18c4d
David L. Tomasovic <davidleo.toma@gmail.com>
2024-09-25 13:08:42 +0200

    Add Footer to site

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="p-4 text-center bg-slate-500">
+      <p className="text-white">
+        &copy; 2023 Your Company Name. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
