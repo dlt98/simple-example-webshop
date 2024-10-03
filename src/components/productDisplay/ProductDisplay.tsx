7cb33cee41a9ac70e4502b4fa68b83379b208e40
David L. Tomasovic <davidleo.toma@gmail.com>
2024-10-03 16:29:56 +0200

    Add empty state to product display

--- src/components/productDisplay/ProductDisplay.tsx
+++ src/components/productDisplay/ProductDisplay.tsx
@@ -3,16 +3,26 @@ import { ProductCard } from "../productCard";
 
 import { useState } from "react";
 import { Pagination, SingleProductModal, Spinner } from "@/components/shared";
+import EmptyContent from "../shared/EmptyContent";
 
 const MAX_PER_PAGE = 20;
 
 export const ProductDisplay = () => {
   const [productId, setProductId] = useState<number | null>(null);
-  const { data, isLoading, setQueryParam } = useGetProductsQuery();
+  const { data, isLoading, setQueryParam, isFetched } = useGetProductsQuery();
 
   const onModalClose = () => {
     setProductId(null);
   };
+
+  if (isFetched && !data?.products.length)
+    return (
+      <EmptyContent
+        icon={<p className="text-4xl">😥</p>}
+        title="Sorry we can't find products"
+        description="We searched far and wide and still couldn't find your products"
+      />
+    );
 
   return (
     <div className="flex flex-col gap-5">
